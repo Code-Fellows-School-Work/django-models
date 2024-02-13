ca43f1442fa84ce12eb4aca9a527c9ff2bb56d13
evidad <errolvidad@gmail.com>
2024-02-13 13:43:16 -0800

    rendered snacks to home page

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import SnackDetailView, SnackListView
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='home'),
-    path('about/', AboutPageView.as_view(), name='about'),
+    path('', SnackListView.as_view(), name='snack_list'),
+    # path('about/', AboutPageView.as_view(), name='about'),
+    path('<int:pk>/', SnackDetailView.as_view(), name='snack_detail')
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -1,7 +1,17 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Snack
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+class SnackListView(ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+    context_object_name = 'snacks'
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
